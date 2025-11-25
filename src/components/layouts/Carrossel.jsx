83c810d2ab0c813fa3c24f27beeff9c9eed3106b
joaoptoaldo <toaldojoaopedro@gmail.com>
2025-11-25 19:42:33 -0300

    commit do projeto

--- src/components/layouts/Carrossel.jsx
+++ src/components/layouts/Carrossel.jsx
@@ -1,6 +1,11 @@
 import Slider from "react-slick";
 import styles from './Carrossel.module.css';
 import anima from '../../styles/animations.module.css';
+import image1 from '../images/image1.jpeg';
+import image2 from '../images/image2.jpg';
+import image3 from '../images/image3.avif';
+import image4 from '../images/image4.jpg';
+
 const Carrossel = () => {
     const settings = {
         dots: true,
@@ -17,16 +22,16 @@ const Carrossel = () => {
         <div className={`${styles.carrossel_container} ${anima.fadeIn}`}>
             <Slider className={styles.slider} {...settings}>
                 <div className={styles.carrossel_img}>
-                    <img src="src/components/images/image1.jpeg" alt="" />
+                    <img src="https://github.com/Joaoptoaldo/card/blob/main/src/components/images/image1.jpeg?raw=true" alt="" />
                 </div>
                 <div className={styles.carrossel_img}>
-                    <img src="src/components/images/image2.jpg" alt="" />
+                    <img src="https://github.com/Joaoptoaldo/card/blob/main/src/components/images/image2.jpg?raw=true" alt="" />
                 </div>
                 <div className={styles.carrossel_img}>
-                    <img src="src/components/images/image3.avif" alt="" />
+                    <img src="https://raw.githubusercontent.com/Joaoptoaldo/card/refs/heads/main/src/components/images/image3.avif" alt="" />
                 </div>
                 <div className={styles.carrossel_img}>
-                    <img src="src/components/images/image4.jpg" alt="" />
+                    <img src="https://github.com/Joaoptoaldo/card/blob/main/src/components/images/image4.jpg?raw=true" alt="" />
                 </div>
             </Slider>
         </div>        
