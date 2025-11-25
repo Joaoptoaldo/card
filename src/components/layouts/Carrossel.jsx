1f94963cf8bf8038dd136c851120a28515096cd2
joaoptoaldo <toaldojoaopedro@gmail.com>
2025-11-25 17:55:06 -0300

    commit do projeto

--- src/components/layouts/Carrossel.jsx
+++ src/components/layouts/Carrossel.jsx
@@ -17,16 +17,16 @@ const Carrossel = () => {
         <div className={`${styles.carrossel_container} ${anima.fadeIn}`}>
             <Slider className={styles.slider} {...settings}>
                 <div className={styles.carrossel_img}>
-                    <img src="https://down-br.img.susercontent.com/file/b38486cac184e56c73c93e869504afc4" alt="" />
+                    <img src="src/components/images/image1.jpeg" alt="" />
                 </div>
                 <div className={styles.carrossel_img}>
-                    <img src="https://i.pinimg.com/736x/6a/6d/69/6a6d69d44a73c53a8225a4a137bd5686.jpg"alt="" />
+                    <img src="src/components/images/image2.jpg" alt="" />
                 </div>
                 <div className={styles.carrossel_img}>
-                    <img src="https://static.wixstatic.com/media/643552_69102a85f55d4385a5fcd7aa75555601~mv2.png/v1/fill/w_484,h_484,al_c,lg_1,q_85,enc_avif,quality_auto/643552_69102a85f55d4385a5fcd7aa75555601~mv2.png" alt="" />
+                    <img src="src/components/images/image3.avif" alt="" />
                 </div>
                 <div className={styles.carrossel_img}>
-                    <img src="https://i.pinimg.com/474x/22/14/be/2214be39b2580b1fbdd8aae918dec291.jpg" alt="" />
+                    <img src="src/components/images/image4.jpg" alt="" />
                 </div>
             </Slider>
         </div>        
