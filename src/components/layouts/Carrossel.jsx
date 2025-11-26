05f867ed33905b81ec1e14441580fe87261449b1
João Pedro Toaldo <109038043+Joaoptoaldo@users.noreply.github.com>
2025-11-26 11:23:23 -0300

    Update Carrossel.jsx

--- src/components/layouts/Carrossel.jsx
+++ src/components/layouts/Carrossel.jsx
@@ -18,16 +18,16 @@ const Carrossel = () => {
         <div className={`${styles.carrossel_container} ${anima.fadeIn}`}>
             <Slider className={styles.slider} {...settings}>
                 <div className={styles.carrossel_img}>
-                    <img src="https://github.com/Joaoptoaldo/card/blob/main/src/components/images/image1.jpeg?raw=true" alt="" />
+                    <img src="https://down-br.img.susercontent.com/file/b38486cac184e56c73c93e869504afc4" alt="" />
                 </div>
                 <div className={styles.carrossel_img}>
-                    <img src="https://github.com/Joaoptoaldo/card/blob/main/src/components/images/image2.jpg?raw=true" alt="" />
+                    <img src="https://down-br.img.susercontent.com/file/b38486cac184e56c73c93e869504afc4" alt="" />
                 </div>
                 <div className={styles.carrossel_img}>
-                    <img src="https://github.com/Joaoptoaldo/card/blob/main/src/components/images/image3.avif?raw=true" alt="" />
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVILWJKf4xQTEoKkigH8O1bCt4LxIBJCtpBrT3357cAbyM898AtoZfTmOg4zkDYvVU3R4&usqp=CAU" alt="" />
                 </div>
                 <div className={styles.carrossel_img}>
-                    <img src="https://github.com/Joaoptoaldo/card/blob/main/src/components/images/image4.jpg?raw=true" alt="" />
+                    <img src="https://i.pinimg.com/736x/22/14/be/2214be39b2580b1fbdd8aae918dec291.jpg" alt="" />
                 </div>
             </Slider>
         </div>        
