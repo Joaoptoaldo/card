19f77a2509651d5789a39c35e70a41d06b651895
joaoptoaldo <toaldojoaopedro@gmail.com>
2025-11-29 20:36:07 -0300

    update da playlist

--- src/components/layouts/SpotifyPlayer.jsx
+++ src/components/layouts/SpotifyPlayer.jsx
@@ -1,4 +1,52 @@
+import React from "react";
+import styles from "./SpotifyPlayer.module.css"; // você pode reutilizar o mesmo CSS
+import anima from "../../styles/animations.module.css";
+
+function SpotifyPlayer() {
+  return (
+    <div className={`${styles.spotifyPlayer} ${anima.slideInTop}`}>
+      <iframe
+        src="https://open.spotify.com/embed/playlist/1l5b5VCePWaBoKhzybA4Sd"
+        frameBorder="0"
+        allowFullScreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        title="Spotify Player"
+      ></iframe>
+    </div>
+  );
+}
+
+export default SpotifyPlayer;
+
+
+
+/*import { useEffect, useRef, useState } from "react";
 import styles from "./SpotifyPlayer.module.css";
+import anima from "../../styles/animations.module.css";
+
+function SpotifyPlayer() {
+  const tracks = [
+    "5wcZuhzXFf7At0N0RmVV6b", // Partilhar — ANAVITÓRIA
+    "5lvvqOEWpUvs2jaFkQTFWJ", // Quando bate aquela saudade - Rubel
+    "1dGr1c8CrMLDpV6mPbImSI", // Lover — Taylor Swift
+    "352FuGmGJClPjojSYjNrXG", // Já sei Namorar — Tribalistas
+    "1W0OCrMsRP1SLTEdw3JwEz", // Sweet Dreams - Last Shadow Puppets
+    "2VyaGJJI8dOiv36F8vHZIL", // Slow Dawn - Chase Atlantic
+    "3U4isOIWM3VvDubwSI3y7a", // All of Me — John Legend
+    "5uCax9HTNlzGybIStD3vDh", // Say You Won’t Let Go — James Arthur
+    "5TTGoX70AFrTvuEtqHK37S", // No1. Party Anthem — Arctic Monkeys
+    "1Q7EgiMOuwDcB0PJC6AzON", // Best Part — Daniel Caesar & H.E.R
+    "3AJwUDP919kvQ9QcozQPxg", // Yellow — Coldplay
+    "28jyMOvRd82hQp95Z9ftXw", // Her - Chase Atlantic
+    "0tgVpDi06FyKpA1z0VMD4v", // Perfect — Ed Sheeran
+    "5XeFesFbtLpXzIVDNQP22n", // I Wanna Be Yours — Arctic Monkeys
+  ];
+
+  const TIMEOUT_MS = 5000; // tempo para esperar o load do iframe antes de tentar próxima faixa
+
+
+/*import styles from "./SpotifyPlayer.module.css";
 import anima from "../../styles/animations.module.css";
 
 function SpotifyPlayer() {
@@ -16,4 +64,4 @@ function SpotifyPlayer() {
   );
 }
 
-export default SpotifyPlayer;
+export default SpotifyPlayer;*/
